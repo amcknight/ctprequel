05f792b09a38a3196b3551036d5d184cdfebf2af
Andrew McKnight <thedonk@gmail.com>
2023-06-28 15:02:41 -0700

    Added Taban to sideviewFair

--- Tools/PIXI/sprites/telepod_crono_standing.asm
+++ Tools/PIXI/sprites/telepod_crono_standing.asm
@@ -6,8 +6,7 @@ PHB : PHK : PLB
 	JSR Graphics
 PLB : RTL
 
-Tiles: db #$00,#$02,#$20,#$22,
-          #$04,#$06,#$24,#$26
+Tiles: db #$0E,#$0C,#$2C
 
 Graphics:
 	%GetDrawInfo()
@@ -16,37 +15,31 @@ Graphics:
   JSR WriteTiles
   PLX
 
-	LDA #$03	; Draw 4 tiles
+	LDA #$02	; Draw 3 tiles
 	LDY #$02	; 16x16 tilesize
 	JSL $01B7B3
 	
   RTS
 
 WriteTiles:
+	%GetDrawInfo()
+
   ; X positions
   LDA $00
   STA $0300,y
-  ADC #$0F
   STA $0304,y
-  LDA $00
   STA $0308,y
-  ADC #$0F
-  STA $030C,y
 
   ; Y positions
   LDA $01
   STA $0301,y
+  ADC #$0F
   STA $0305,y
   ADC #$0F
   STA $0309,y
-  STA $030D,y
 
   ; Tiles
-  LDA $14        ; Counter for frame switching
-  AND #$10       ; Every 2^4 frames
-  LSR : LSR : LSR : LSR; Move bit to lowest bit
-  ASL : ASL      ; Multiply by 4 because 4 tiles per frame
-  TAX            ; Set masked counter in X register for indexing (0 or 4)
+  LDX #$00
   LDA Tiles,x
   STA $0302,y
   INX
@@ -54,16 +47,12 @@ WriteTiles:
   STA $0306,y
   INX
   LDA Tiles,x
-  STA $030A,Y
-  INX
-  LDA Tiles,x
-  STA $030E,y
+  STA $030A,y
 
   ; Properties
-  LDA #$39
+  LDA #$35 ; YXPPCCCT
   STA $0303,y
   STA $0307,y
   STA $030B,y
-  STA $030F,y
   
   RTS
--- Tools/PIXI/sprites/telepod_lucca.asm
+++ Tools/PIXI/sprites/telepod_lucca.asm
@@ -6,31 +6,53 @@ PHB : PHK : PLB
 	JSR Graphics
 PLB : RTL
 
+Tiles: db #$40,#$60,
+          #$42,#$62,
+          #$44,#$64,
+          #$42,#$62
+
 Graphics:
 	%GetDrawInfo()
 
-	LDA $00
-	STA $0300,y	; X position
-	LDA $01
-	STA $0301,y	; Y position
-	LDA #$40
-	STA $0302,y	; Tile number
-	LDA #$37
-	STA $0303,y	; Properties
+  PHX
+  JSR WriteTiles
+  PLX
 
-	LDA $00
-	STA $0304,y	; X position
-	LDA $01
-	ADC #$0F
-	STA $0305,y	; Y position
-	LDA #$60
-	STA $0306,y	; Tile number
-	LDA #$37
-	STA $0307,y	; Properties
-
-	; Draw it
-
-	LDA #$01	; Draw 16 tiles
+	LDA #$01	; Draw 2 tiles
 	LDY #$02	; 16x16 tilesize
 	JSL $01B7B3
-	RTS
+	
+  RTS
+
+WriteTiles:
+	%GetDrawInfo()
+
+  ; X positions
+  LDA $00
+  STA $0300,y
+  STA $0304,y
+
+  ; Y positions
+  LDA $01
+  STA $0301,y
+  ADC #$0F
+  STA $0305,y
+
+  ; Tiles
+  LDA $14        ; Counter for frame switching
+  AND #$08       ; Every 2^3 frames
+  LSR : LSR : LSR; Move bit to lowest bit
+  ASL            ; Multiply by 2 because 2 tiles per frame
+  TAX            ; Set masked counter in X register for indexing (0 or 4)
+  LDA Tiles,x
+  STA $0302,y
+  INX
+  LDA Tiles,x
+  STA $0306,y
+
+  ; Properties
+  LDA #$37
+  STA $0303,y
+  STA $0307,y
+  
+  RTS
